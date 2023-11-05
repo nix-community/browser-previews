1a57644a86b839774a78e1f68b3ef5e22ab16a27
Robert K. Bell <robert.k.bell@gmail.com>
2023-11-05 14:26:06 +1100

    ci: test the auto version update

--- google-chrome/upstream-info.nix
+++ google-chrome/upstream-info.nix
@@ -1,11 +1,11 @@
 {
   beta = {
     hash_deb_amd64 = "sha256-HV2JqmpFuLqSEHbZrrG1A5t1PhuOEhIDkYEy9kxEwg0=";
-    version = "120.0.6099.5";
+    version = "120.0.6099.4";
   };
   dev = {
     hash_deb_amd64 = "sha256-GrQeNKnnDO6l9XVyCvJ8D0Q8cdWxDVUrnkb7rgpgyEc=";
-    version = "120.0.6090.0";
+    version = "120.0.6089.0";
   };
   stable = {
     chromedriver = {
@@ -16,6 +16,6 @@
       version = "119.0.6045.105";
     };
     hash_deb_amd64 = "sha256-WLTTFMUvtBHvvegDFpZ+7Eht9StMyleaqXEBhPhgPTs=";
-    version = "119.0.6045.105";
+    version = "119.0.6045.104";
   };
 }
