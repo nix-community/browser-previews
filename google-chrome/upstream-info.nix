e954f130535809e271c6e4e1b59fccf1a64ee524
BumpBot <r-k-b@users.noreply.github.com>
2023-11-28 22:41:12 +0000

    chrome: 119.0.6045.159 -> 119.0.6045.199
    https://chromereleases.googleblog.com/2023/11/stable-channel-update-for-desktop_28.html
    
    This update includes 7 security fixes. Google is aware that an exploit
    for CVE-2023-6345 exists in the wild.
    
    CVEs:
    CVE-2023-6348 CVE-2023-6347 CVE-2023-6346 CVE-2023-6350 CVE-2023-6351
    CVE-2023-6345

--- google-chrome/upstream-info.nix
+++ google-chrome/upstream-info.nix
@@ -15,7 +15,7 @@
       hash_linux = "sha256-2o6LAo2pEsCi1exPv1nEMk2Tklhh49UyWaYoyQ7Df/Y=";
       version = "119.0.6045.105";
     };
-    hash_deb_amd64 = "sha256-xAm7bPsnnJD7UWNTtHKMv5enHo3rM9w0M81QPqZVlP4=";
-    version = "119.0.6045.159";
+    hash_deb_amd64 = "sha256-9nZjyJnXvOO1iZea3mdsj5FYkylrWnhColZ+q+X/xcU=";
+    version = "119.0.6045.199";
   };
 }
