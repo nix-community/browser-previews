ede15895ed40cb519ff5c01ba9046420fcc4c81d
BumpBot <r-k-b@users.noreply.github.com>
2023-12-06 19:22:36 +0000

    chrome: 119.0.6045.199 -> 120.0.6099.62
    https://chromereleases.googleblog.com/2023/12/stable-channel-update-for-desktop.html
    
    This update includes 10 security fixes.
    
    CVEs:
    CVE-2023-6508 CVE-2023-6509 CVE-2023-6510 CVE-2023-6511 CVE-2023-6512

--- google-chrome/upstream-info.nix
+++ google-chrome/upstream-info.nix
@@ -9,13 +9,13 @@
   };
   stable = {
     chromedriver = {
-      hash_darwin = "sha256-sRAU9RJANz+Sov6oaoZasMoaqM+mIZSDbag92wXsVCI=";
+      hash_darwin = "sha256-Sj/DKPVSRe+Va4Xbn4NM3wzoq0l0hJXgCELTM9Gh3Js=";
       hash_darwin_aarch64 =
-        "sha256-U+PBsfpc7PNZYedHIdPnWXA9xKpRnon5vxgKKJr69ow=";
-      hash_linux = "sha256-2o6LAo2pEsCi1exPv1nEMk2Tklhh49UyWaYoyQ7Df/Y=";
-      version = "119.0.6045.105";
+        "sha256-lo0Mpx9MbMvuI/dDXcoh5B01eWZM9187e/egtUoGAbY=";
+      hash_linux = "sha256-JXA7CV2KBYZBP+NSnqYq6U28MT5TIOgEH7RDuhbloBo=";
+      version = "120.0.6099.62";
     };
-    hash_deb_amd64 = "sha256-9nZjyJnXvOO1iZea3mdsj5FYkylrWnhColZ+q+X/xcU=";
-    version = "119.0.6045.199";
+    hash_deb_amd64 = "sha256-bgp2K7jmOZxUC6k5Ozga3eGOIin3rE8d+SIy2ZNC8GE=";
+    version = "120.0.6099.62";
   };
 }
