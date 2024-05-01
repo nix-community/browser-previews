afbc79a232421df6f88e8e48c0a044cb967c340e
BumpBot <r-k-b@users.noreply.github.com>
2024-05-01 19:25:28 +0000

    chrome: 124.0.6367.91 -> 124.0.6367.118
    https://chromereleases.googleblog.com/2024/04/stable-channel-update-for-desktop_30.html
    
    This update includes 2 security fixes.
    
    CVEs:
    CVE-2024-4331 CVE-2024-4368

--- google-chrome/upstream-info.nix
+++ google-chrome/upstream-info.nix
@@ -10,12 +10,11 @@
   stable = {
     chromedriver = {
       hash_darwin = "sha256-RGOChK4JhrFUgVY/5YqgE0KFLRl6a7X2llw1ZfhiPXY=";
-      hash_darwin_aarch64 =
-        "sha256-K1jFXmWtXrS43UJg2mQ39Kae6tv7E9Fxm6LUWg+uwLo=";
+      hash_darwin_aarch64 = "sha256-K1jFXmWtXrS43UJg2mQ39Kae6tv7E9Fxm6LUWg+uwLo=";
       hash_linux = "sha256-xwaRNh7sllyNaq8+aLAZDQ3uDg06cu3KYqc02LWPSyw=";
       version = "124.0.6367.91";
     };
-    hash_deb_amd64 = "sha256-CyCbZQ5ce8WLTt2JVSqbDkLDboE4BloiZ8pJff3dmSY=";
-    version = "124.0.6367.91";
+    hash_deb_amd64 = "sha256-H3bv6WiVBl4j38ROZ80+SD9UO9ok+xxcKFxDd9yjWNY=";
+    version = "124.0.6367.118";
   };
 }
