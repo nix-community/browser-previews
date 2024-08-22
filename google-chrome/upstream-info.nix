5b3e1884cb5dbc455cf9803f1f8d54baf0c97108
BumpBot <r-k-b@users.noreply.github.com>
2024-08-22 19:25:23 +0000

    chrome: 127.0.6533.119 -> 128.0.6613.84
    https://chromereleases.googleblog.com/2024/08/stable-channel-update-for-desktop_21.html
    
    This update includes 38 security fixes. Google is aware that an exploit
    for CVE-2024-7971 exists in the wild.
    
    CVEs:
    CVE-2024-7964 CVE-2024-7965 CVE-2024-7966 CVE-2024-7967 CVE-2024-7968
    CVE-2024-7969 CVE-2024-7971 CVE-2024-7972 CVE-2024-7973 CVE-2024-7974
    CVE-2024-7975 CVE-2024-7976 CVE-2024-7977 CVE-2024-7978 CVE-2024-7979
    CVE-2024-7980 CVE-2024-7981 CVE-2024-8033 CVE-2024-8034 CVE-2024-8035

--- google-chrome/upstream-info.nix
+++ google-chrome/upstream-info.nix
@@ -9,12 +9,12 @@
   };
   stable = {
     chromedriver = {
-      hash_darwin = "sha256-z+08R1CYi53edNEDSvJ303+hc/pqiFvVhUkD+g4q6tE=";
-      hash_darwin_aarch64 = "sha256-q60NWIka3QzLsWg3X/+qTWby6UTf7wvjCDdZWlUjzPA=";
-      hash_linux = "sha256-6E6LaQCx+2Xd/vjjaiQR3vJgkP7L4MRkA7Bb0jTrakc=";
-      version = "127.0.6533.119";
+      hash_darwin = "sha256-4wp3nlGkuNDlmF+3bmJOmaMccQcsXBZaVO95Se6Vj1M=";
+      hash_darwin_aarch64 = "sha256-La32ZBMgdWyl4nFoh4LfaxsoZJxrYkB/fbYOzltG8e8=";
+      hash_linux = "sha256-qhoTtgPNrs2jMEVbVuVZAsQDygm72xfhWvhDC/mDUzc=";
+      version = "128.0.6613.84";
     };
-    hash_deb_amd64 = "sha256-k9rsELAtOFdLSi1dOTV4Lr7E2Uu5sR1/GOL9BWDqZl4=";
-    version = "127.0.6533.119";
+    hash_deb_amd64 = "sha256-4hABtHDq/8lIP6YbpU1VJdmgZNmLN2qb0VMPYw3dsBg=";
+    version = "128.0.6613.84";
   };
 }
