cf49273a7e76e57944cb5423aa29154e398855e5
BumpBot <r-k-b@users.noreply.github.com>
2025-02-12 19:25:33 +0000

    chrome: 133.0.6943.53 -> 133.0.6943.98
    https://chromereleases.googleblog.com/2025/02/stable-channel-update-for-desktop_12.html
    
    This update includes 4 security fixes.
    
    CVEs:
    CVE-2025-0995 CVE-2025-0996 CVE-2025-0997 CVE-2025-0998

--- google-chrome/upstream-info.nix
+++ google-chrome/upstream-info.nix
@@ -9,12 +9,12 @@
   };
   stable = {
     chromedriver = {
-      hash_darwin = "sha256-GdGqwiK73hEQzbVLFEEOpiq5pidBGJUndkTI8GOKXTU=";
-      hash_darwin_aarch64 = "sha256-O1OzKrW2VjVHmIFfMMJwZUucdmSvW6H9SZP1yzV1c7c=";
-      hash_linux = "sha256-UgHVGzyM4Th75jPk3eS6nqxYf8ZJRhlbJkKXsP4yiJg=";
-      version = "133.0.6943.53";
+      hash_darwin = "sha256-8CamWPjcWk4ZmgkyCD96VtSesa4K/FZe8Uvo22jZ3HU=";
+      hash_darwin_aarch64 = "sha256-yZMZYAb33J/pQBqsdho0pB+HsxFxNPoyD+HCfTS+7YQ=";
+      hash_linux = "sha256-cQPneSI/DU+el6WDcI5YmtdmIkdeE0b9s7IjaU1YJF0=";
+      version = "133.0.6943.98";
     };
-    hash_deb_amd64 = "sha256-73X2cohboI+GbFDk5ikjxnT1PKjG4UoyzhA9T9Qe9Vk=";
-    version = "133.0.6943.53";
+    hash_deb_amd64 = "sha256-UHpt9ToxrE2VA3c5+zOpMYpKWPRWIIeCErIZwEWkfqg=";
+    version = "133.0.6943.98";
   };
 }
