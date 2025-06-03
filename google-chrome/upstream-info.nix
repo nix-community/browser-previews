968d559f159cd908ba8dfd2f174276f9c4173a47
BumpBot <r-k-b@users.noreply.github.com>
2025-06-03 19:40:17 +0000

    chrome: 137.0.7151.55 -> 137.0.7151.68
    https://chromereleases.googleblog.com/2025/06/stable-channel-update-for-desktop.html
    
    This update includes 3 security fixes. Google is aware that an exploit
    for CVE-2025-5419 exists in the wild.
    
    CVEs:
    CVE-2025-5419 CVE-2025-5068

--- google-chrome/upstream-info.nix
+++ google-chrome/upstream-info.nix
@@ -9,12 +9,12 @@
   };
   stable = {
     chromedriver = {
-      hash_darwin = "sha256-imx2zeQSdfU52uHJKHxQ3vZIHxdze22HEjfoHCbtpjU=";
-      hash_darwin_aarch64 = "sha256-AIziso+PmMV2U7lAqIgqYSGYo8gUrI7hfhR4Ze9HctI=";
-      hash_linux = "sha256-V7RKa1GTURiY6xwms+roN4p2Om0J52h7gBp5nVjn6Q0=";
-      version = "137.0.7151.55";
+      hash_darwin = "sha256-82lAakjKmO38GEXjkAvgOatMk96r6UEohRS7QCYxpcE=";
+      hash_darwin_aarch64 = "sha256-vCNyg+/u2wKgZTYevbSZ2Z7DQ08ZwDkA3MYTMBpM3/0=";
+      hash_linux = "sha256-BeNWs91Jeib140EsZ7WZdoU0WPGzqlsrn2Bs7N+SJYY=";
+      version = "137.0.7151.68";
     };
-    hash_deb_amd64 = "sha256-Q4zf60OQN/2NRozssVrnmbYWGRm05Mt2/6LozfENzgM=";
-    version = "137.0.7151.55";
+    hash_deb_amd64 = "sha256-TBJ6XOnxrP/+Gnd2Y3/aYbRG5N+b7dKS/u80ZK4E+MM=";
+    version = "137.0.7151.68";
   };
 }
