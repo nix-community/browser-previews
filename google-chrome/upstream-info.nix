e46e93d0e728165272287d65e99ec988aae32e8c
BumpBot <r-k-b@users.noreply.github.com>
2025-10-10 19:27:15 +0000

    chrome: 141.0.7390.65 -> 141.0.7390.76
    https://chromereleases.googleblog.com/2025/10/stable-channel-update-for-desktop_9.html

--- google-chrome/upstream-info.nix
+++ google-chrome/upstream-info.nix
@@ -9,12 +9,12 @@
   };
   stable = {
     chromedriver = {
-      hash_darwin = "sha256-h3ZlflcYcB+fE3QOBO+8/xywi8/Mx9RZ2IAId1wF2eY=";
-      hash_darwin_aarch64 = "sha256-CM+1IDBQgjCRv6b6EW09STRxmol8TcPZoByW0m/duqQ=";
-      hash_linux = "sha256-6rzSNPUWu4XO7rk2j2lKO307PyAC038qmG8aISvJ9pY=";
-      version = "141.0.7390.65";
+      hash_darwin = "sha256-RKHLP/2VyWgs13gSQO2oFUAhF0n8aHkKgu0KrPb+IpM=";
+      hash_darwin_aarch64 = "sha256-2Ds0VcZ5J2G464i399uxcih3A6r4SB18rGPmaLbeJyM=";
+      hash_linux = "sha256-pLe2+jbx50zaqVGPDOh041wjyVmorDdBkmHYzTvnAIU=";
+      version = "141.0.7390.76";
     };
-    hash_deb_amd64 = "sha256-Vdjrfq53R6aPlH3jM3A8qJnlsUJs6mac/r+NAZqWlq0=";
-    version = "141.0.7390.65";
+    hash_deb_amd64 = "sha256-YSvDdI1TjdVn8EgKUpU4cWpFEhBzHE0w4Eq7xZBeBcc=";
+    version = "141.0.7390.76";
   };
 }
