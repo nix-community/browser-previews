3af632a0828505c21b67c6be50df099c0deb457a
BumpBot <r-k-b@users.noreply.github.com>
2026-02-24 19:53:54 +0000

    chrome: 145.0.7632.109 -> 145.0.7632.116
    https://chromereleases.googleblog.com/2026/02/stable-channel-update-for-desktop_23.html
    
    This update includes 3 security fixes.
    
    CVEs:
    CVE-2026-3061 CVE-2026-3062 CVE-2026-3063

--- google-chrome/upstream-info.nix
+++ google-chrome/upstream-info.nix
@@ -9,12 +9,12 @@
   };
   stable = {
     chromedriver = {
-      hash_darwin = "sha256-lC4W+L2zEW7mNZ/TtIFtXAuihEppNY99rOH+narNa6k=";
-      hash_darwin_aarch64 = "sha256-nhw0sb4AZ6pNhlDGKdYMRbMKbDr4jqrbYptAkv5UmUw=";
-      hash_linux = "sha256-gvnhEolG8FOGe4uTms9YkvHwN9K59zAEb0tFCCDEXUc=";
-      version = "145.0.7632.77";
+      hash_darwin = "sha256-/qYIZ7EqPMQga6TJ3qSdmrbvJxZTpFBnVSbMmvwJ8NQ=";
+      hash_darwin_aarch64 = "sha256-JdCeu8qQYotIDpXg0VQYGPjnZG0Ld/qBxiWPwTwGE9A=";
+      hash_linux = "sha256-fzl/pxYUstl9ywaofzKXyRclBCgDZU1sf+3NEDh6Xfg=";
+      version = "145.0.7632.117";
     };
-    hash_deb_amd64 = "sha256-VNvx/3l3gzj8HjeeK3v/Hj05E9t2PvsiP8pnIOq1AEA=";
-    version = "145.0.7632.109";
+    hash_deb_amd64 = "sha256-lGMopGtpLp3g0PVIfRIACNP6yRarzQDIsuctNbiqCCo=";
+    version = "145.0.7632.116";
   };
 }
