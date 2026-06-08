1e57ade7a6b3c916b0f89a695ed9dd257ec67492
BumpBot <r-k-b@users.noreply.github.com>
2026-06-08 21:18:26 +0000

    chrome: 149.0.7827.53 -> 149.0.7827.102
    https://chromereleases.googleblog.com/2026/06/stable-channel-update-for-desktop_0153744567.html

--- google-chrome/upstream-info.nix
+++ google-chrome/upstream-info.nix
@@ -9,12 +9,12 @@
   };
   stable = {
     chromedriver = {
-      hash_darwin = "sha256-3S+crqS6/Nw0LwLvMYcVNRWT8MVjkSRC3UTkHZqyiHw=";
-      hash_darwin_aarch64 = "sha256-WMuBGIFy7PLbQfdwRTiUIBfryeG9uCIVH5chxwZoScI=";
-      hash_linux = "sha256-J6JdJwGVVSp15yyG1CWznkvR2lHXpt+fyQcWKeOgrL8=";
-      version = "149.0.7827.54";
+      hash_darwin = "sha256-WzOYcQzVyJksxejja2LStIdvQB8nx7BUSt6/PHep9fE=";
+      hash_darwin_aarch64 = "sha256-+bacsdHpYWJER1ivAnPeyFTR/h4dKwpgR7PKgn+FtQc=";
+      hash_linux = "sha256-hoSpyweTkTUgIek+bdsvKvpm/rMnUm98PtMWtbECiq8=";
+      version = "149.0.7827.55";
     };
-    hash_deb_amd64 = "sha256-iqNNjJy9Wje5jcpJrQYHu/gZptaBwZg0WZzbZTKUmPg=";
-    version = "149.0.7827.53";
+    hash_deb_amd64 = "sha256-ETp7EURoN0p45cNnwcp6/aoPyKHSf+WPyUvKh+R7bp4=";
+    version = "149.0.7827.102";
   };
 }
