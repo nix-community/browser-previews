c4ec9eada3ec5de32b2faa265554a37396000c5d
BumpBot <r-k-b@users.noreply.github.com>
2026-08-19 19:43:28 +0000

    chrome: 151.0.7922.137 -> 151.0.7922.169
    http://www.google.com/sorry/index
    
    This update includes 15 security fixes.
    
    CVEs:
    CVE-2026-76034 CVE-2026-76036 CVE-2026-76033 CVE-2026-76037
    CVE-2026-76044 CVE-2026-76039 CVE-2026-76040 CVE-2026-76035
    CVE-2026-76042 CVE-2026-76046 CVE-2026-76043 CVE-2026-76041
    CVE-2026-76047 CVE-2026-76038 CVE-2026-76045

--- google-chrome/upstream-info.nix
+++ google-chrome/upstream-info.nix
@@ -9,12 +9,12 @@
   };
   stable = {
     chromedriver = {
-      hash_darwin = "sha256-6BDRsVFMSnbNgj27CSfxl0JhJllV+wIOsz0IBg1RJQE=";
-      hash_darwin_aarch64 = "sha256-cBnXPa0gwLhTYpMLzmT7tRTTP5kQIMr9g8C2exNAtk0=";
-      hash_linux = "sha256-X5Vqww1ceEU/XrV4/qFMsJyvYrsFFBQpvkOvCwyZIvg=";
-      version = "151.0.7922.138";
+      hash_darwin = "sha256-TYytK7p52B64QUa/tx+Q/mW5NnnwMX7HoKM1tD790Gs=";
+      hash_darwin_aarch64 = "sha256-NWwpskp7a48pCtAxKxzNkOOVmB26V74uCsGAMul3r1Y=";
+      hash_linux = "sha256-ZvOYTWZt6EVXBt6JaL16+2rT+nDBoICEu6gjXhRmF6Y=";
+      version = "152.0.7977.54";
     };
-    hash_deb_amd64 = "sha256-5tq/BEz5zQJ5z+hu+kMWgsGL/AbQYznOBVqqh66HFyc=";
-    version = "151.0.7922.137";
+    hash_deb_amd64 = "sha256-ZXJHgxBVPLJf3LS6L7VFm0csLHZfX2jpg31JZOiofx4=";
+    version = "151.0.7922.169";
   };
 }
